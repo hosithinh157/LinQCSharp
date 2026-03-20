45e6889a81240d8ac51d39fd21d2b8f6e0500815
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-20 23:35:55 +0700

    Add files via upload

---
+++ Bai5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuchanhCS
+{
+    class Bai5
+    {
+        public static void Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+            List<int> numbers = new List<int>();
+
+            Console.Write("\nNhap so luong phan tu: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write($"Nhap phan tu thu {i + 1}: ");
+                    if (int.TryParse(Console.ReadLine(), out int num))
+                    {
+                        numbers.Add(num);
+                    }
+                }
+
+                var squaredNumbers = numbers.Select(x => x * x).ToList();
+
+                Console.WriteLine("\nDanh sach binh phuong cua cac so:");
+                Console.WriteLine(string.Join(" ", squaredNumbers));
+            }
+            else
+            {
+                Console.WriteLine("So luong khong hop le!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
