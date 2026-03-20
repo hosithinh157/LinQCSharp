c4a445d6d56451445ab372eb009c6704b066d435
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-03-20 23:34:45 +0700

    Add files via upload

---
+++ Bai1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuchanhCS
+{
+    public class Bai1
+    {
+        public static void Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hồ Sĩ Thịnh");
+            Console.Write("\nMSV: 23115053122239");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+            Console.WriteLine("\nDanh sach goc: " + string.Join(", ", numbers));
+            Console.WriteLine("Cac so chan la: " + string.Join(", ", evenNumbers));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace ThuchanhCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            int choice;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("===== MENU CHUONG TRINH =====");
+                Console.WriteLine("1. Bai 1");
+                Console.WriteLine("2. Bai 2");
+                Console.WriteLine("3. Bai 3");
+                Console.WriteLine("4. Bai 4");
+                Console.WriteLine("5. Bai 5");
+                Console.WriteLine("6. Bai 6");
+                Console.WriteLine("7. Bai 7");
+                Console.WriteLine("0. Thoát");
+                Console.Write("Chon bai: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Bai1.Run();
+                        break;
+                    case 2:
+                        Bai2.Run();
+                        break;
+                    case 3:
+                        Bai3.Run();
+                        break;
+                    case 4:
+                        Bai4.Run();
+                        break;
+                    case 5:
+                        Bai5.Run();
+                        break;
+                    case 6:
+                        Bai6.Run();
+                        break;
+                    case 7:
+                        Bai7.Run();
+                        break;
+                    case 0:
+                        Console.WriteLine("Thoat chuong trinh...");
+                        break;
+                    default:
+                        Console.WriteLine("Khong hop le");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
